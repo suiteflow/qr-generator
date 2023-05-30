6024be77ad069110d378a53422256dc0457d7b7f
Aaron Grieve <127838213+suiteflow@users.noreply.github.com>
2023-05-30 12:55:22 +0100

    Update qr-generator.js

--- qr-generator.js
+++ qr-generator.js
@@ -1,4 +1,4 @@
   $(document).ready(function(){
     $('[suite-qr="code"]').qrcode({width: 128,height: 128,text: window.location.href});
-    $('[suite-qr="code"] img').css({'width': '100%', 'height': '100%'});
+    $('[suite-qr="code"] canvas').css({'width': '100%', 'height': '100%'});
   });
